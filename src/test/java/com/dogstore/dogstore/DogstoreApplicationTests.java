46a6bb59f57bc0751660f7ef027240f51df0a754
Victor <aue228lublupacanov@gmail.com>
2023-10-25 11:53:17 +0300

    first commit, project created

---
+++ src/test/java/com/dogstore/dogstore/DogstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dogstore.dogstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
