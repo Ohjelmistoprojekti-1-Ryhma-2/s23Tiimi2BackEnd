5e94dcd5c5dd1ea3cc1656bdd17a6c32f7093aa1
Victor <aue228lublupacanov@gmail.com>
2023-11-06 17:29:43 +0200

    created rest all product interface, all manufacturer interface, TODO: productType interface

--- src/main/java/com/dogstore/dogstore/controllers/RestController.java
+++ src/main/java/com/dogstore/dogstore/controllers/RestController.java
@@ -1,14 +1,18 @@
 package com.dogstore.dogstore.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.dogstore.dogstore.models.Manufacturer;
 import com.dogstore.dogstore.models.Product;
+import com.dogstore.dogstore.repository.ManufacturerRepository;
 import com.dogstore.dogstore.repository.ProductRepository;
 
 @Controller
@@ -16,36 +20,56 @@ public class RestController {
 
     @Autowired
     private ProductRepository productRepository;
+    @Autowired
+    private ManufacturerRepository manufacturerRepository;
 
-    // REST interface
+    // REST Products interface
     @RequestMapping(value = "/products", method = RequestMethod.GET)
     public @ResponseBody List<Product> productsListRest() {
         return (List<Product>) productRepository.findAll();
     }
+    /*
+     * TODO REST Products by product type interface
+     * 
+     * @RequestMapping(value = "/products/jacket", method = RequestMethod.GET)
+     * public @ResponseBody List<Product> getJacketProducts() {
+     * return (List<Product>) productRepository.find;
+     * }
+     */
 
-    // REST PUT
-    @RequestMapping(value = "/editproducts", method = RequestMethod.PUT)
-    public @ResponseBody Product saveProductRest(Product product) {
-        return productRepository.save(product);
+    // REST Manufacturer interface
+    @RequestMapping(value = "/manufacturer", method = RequestMethod.GET)
+    public @ResponseBody List<Manufacturer> manufacturersListRest() {
+        return (List<Manufacturer>) manufacturerRepository.findAll();
     }
 
-    // REST DELETE
-    @RequestMapping(value = "/deleteproducts", method = RequestMethod.DELETE)
-    public @ResponseBody String deleteProductRest(Long id) {
-        productRepository.deleteById(id);
-        return "Deleted";
-    }
-
-    // REST POST
-    @RequestMapping(value = "/addproducts", method = RequestMethod.POST)
-    public @ResponseBody Product addProductRest(Product product) {
-        return productRepository.save(product);
-    }
-
-    // REST GET
-    @RequestMapping(value = "/products/{id}", method = RequestMethod.GET)
-    public @ResponseBody Product findProductRest(Long id) {
-        return productRepository.findById(id).get();
-    }
+    /*
+     * @RequestMapping(value = "/product/{id}", method = RequestMethod.GET)
+     * public @ResponseBody Optional<Product> findProductRest(@PathVariable("id")
+     * Long id) {
+     * return productRepository.findById(id);
+     * }
+     * // REST PUT
+     * 
+     * @RequestMapping(value = "/editproducts", method = RequestMethod.PUT)
+     * public @ResponseBody Product saveProductRest(Product product) {
+     * return productRepository.save(product);
+     * }
+     * 
+     * // REST DELETE
+     * 
+     * @RequestMapping(value = "/deleteproducts", method = RequestMethod.DELETE)
+     * public @ResponseBody String deleteProductRest(Long id) {
+     * productRepository.deleteById(id);
+     * return "Deleted";
+     * }
+     * 
+     * // REST POST
+     * 
+     * @RequestMapping(value = "/addproducts", method = RequestMethod.POST)
+     * public @ResponseBody Product addProductRest(Product product) {
+     * return productRepository.save(product);
+     * }
+     */
 
 }
--- src/main/java/com/dogstore/dogstore/repository/ProductRepository.java
+++ src/main/java/com/dogstore/dogstore/repository/ProductRepository.java
@@ -3,6 +3,7 @@ package com.dogstore.dogstore.repository;
 import org.springframework.data.repository.CrudRepository;
 
 import com.dogstore.dogstore.models.Product;
+import java.util.List;
 
 public interface ProductRepository extends CrudRepository<Product, Long> {
 
