c740ee5d068c6f4b315f8d1247f1b2563118fdee
Kaj Jansson <kaj.jansson@myy.haaga-helia.fi>
2023-11-08 13:23:55 +0200

    Commit manufacturer

--- src/main/java/com/dogstore/dogstore/repository/ProductRepository.java
+++ src/main/java/com/dogstore/dogstore/repository/ProductRepository.java
@@ -1,10 +1,11 @@
 package com.dogstore.dogstore.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.dogstore.dogstore.models.Product;
-import java.util.List;
 
+@RepositoryRestResource
 public interface ProductRepository extends CrudRepository<Product, Long> {
 
 }
