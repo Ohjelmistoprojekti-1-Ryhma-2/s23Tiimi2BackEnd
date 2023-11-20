0d2c656399c8d9355a06c6911ddc230f8eeafc96
Kaj Jansson <kaj.jansson@myy.haaga-helia.fi>
2023-11-20 06:31:14 +0200

    Customer.java added

---
+++ src/main/java/com/dogstore/dogstore/models/Customer.java
@@ -0,0 +1,119 @@
+package com.dogstore.dogstore.models;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+/*
+ * This entity is for listing and searching
+ *  all registered customers.
+ */
+
+public class Customer {
+	// Automatic running Id
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	// Essential informative parameters about manufactures
+
+	@NotBlank(message = "Name is mandatory")
+	private String name;
+
+	// @NotBlank(message = "Address is mandatory")
+	private String address;
+
+	@NotBlank(message = "Phone is mandatory")
+	private String phone;
+
+	@NotBlank(message = "Email is mandatory")
+	@Email(message = "Invalid email address")
+	private String email;
+
+	public Customer() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Customer(Long id, String name, String address, String phone, String email) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.address = address;
+		this.phone = phone;
+		this.email = email;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public Long getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the address
+	 */
+	public String getAddress() {
+		return address;
+	}
+
+	/**
+	 * @param address the address to set
+	 */
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	/**
+	 * @return the phone
+	 */
+	public String getPhone() {
+		return phone;
+	}
+
+	/**
+	 * @param phone the phone to set
+	 */
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
