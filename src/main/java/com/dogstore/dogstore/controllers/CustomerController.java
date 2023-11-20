ba89b8109b3e44321704c080d1a843f89dd06bcb
Kaj Jansson <kaj.jansson@myy.haaga-helia.fi>
2023-11-20 13:25:22 +0200

    CustomerController updated

--- src/main/java/com/dogstore/dogstore/controllers/CustomerController.java
+++ src/main/java/com/dogstore/dogstore/controllers/CustomerController.java
@@ -2,21 +2,30 @@ package com.dogstore.dogstore.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import com.dogstore.dogstore.repository.CustomerRepository;
-
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.dogstore.dogstore.models.Customer;
+import com.dogstore.dogstore.repository.CustomerRepository;
 
 @Controller
 public class CustomerController {
 
-    @Autowired
-    private CustomerRepository customerRepository;
+	@Autowired
+	private CustomerRepository customerRepository;
 
-    @GetMapping("/customers")
-    public String listOfCustomers(Model model) {
-        model.addAttribute("customers", customerRepository.findAll());
-        return "listcustomers";
-    }
+	// Redirect to list of registered customer page.
+	@GetMapping("/listcustomers")
+	public String listOfCustomers(Model model) {
+		model.addAttribute("customers", customerRepository.findAll());
+		return "listcustomers";
+	}
+
+	// Move to /addcustomer -enpoint,
+	// which has a form for new customers to register.
+	@GetMapping("/addcustomer")
+	public String addCustomerForm(Model model) {
+		model.addAttribute("Customer", new Customer());
+		return "addcustomer";
+	}
 }
